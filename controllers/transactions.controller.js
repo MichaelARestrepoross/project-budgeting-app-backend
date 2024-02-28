5d0c8dd5d9f736cd7d1cde94d7479ec23faa1994
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-02-28 12:36:41 -0500

    Updated transcationController with id show route

--- controllers/transactions.controller.js
+++ controllers/transactions.controller.js
@@ -7,10 +7,26 @@ const transactions = express.Router()
 //the data
 let transactionsArray = require('../models/transactions.model')
 
-//creeate get route to return json data to the client
+//create get route to return json data to the transaction
 transactions.get('/', (req, res) => {
     res.json({ transactions : transactionsArray })
 })
+
+
+//get a single transaction by id
+transactions.get('/:id', (req, res) => {
+  const { id } = req.params
+
+  isId = transactionsArray.find((t) => t.id === +id)
+
+  if(isId){
+    const transaction = transactionsArray.find((t)=> t.id === +id)
+    res.json({ transaction })
+  }else{
+    
+    res.status(404).json({ status:404 , error: `Transaction with the ID ${id} not found` });
+  }
+});
 
 
 module.exports = transactions
