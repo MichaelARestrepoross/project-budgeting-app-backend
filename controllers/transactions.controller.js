aca895538dfd9990561ddaafdda167e5e4c796bd
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-02-28 11:43:54 -0500

    created basicp ath for transactions data

--- controllers/transactions.controller.js
+++ controllers/transactions.controller.js
@@ -1,5 +1,14 @@
 // we need to import express 
 const express = require('express')
 
+//we need to create router which is a way to refrence in app.js this file
+const transactions = express.Router()
 
+//the data
+let transactionsArray = require('../models/transactions.model')
+
+//creeate get route to return json data to the client
+transactions.get('/', (req, res) => {
+    res.json({ transactions : transactionsArray })
+})
   
